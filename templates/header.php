7ede11180b9f09479a7ca857dd973694d4b41d24
mhadih <m.hadi.hojjat@gmail.com>
2019-01-26 16:28:47 +0330

    change logo

--- templates/header.php
+++ templates/header.php
@@ -26,7 +26,7 @@
                 <a href="<?php echo GUI::path() ?>">
                     <?php GUI::includeLogo() ?>
 
-                    <?php if(Config::get('site_name_in_header')) { ?>
+                    <?php if(!Config::get('site_name_in_header')) { ?>
                         <div class="col-2 offset-5"><img src="../www/images/ut_logo.png" class="img-fluid"></div>
                     <?php } ?>
                     
