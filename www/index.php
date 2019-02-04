4a50f239a6c7b855e787bb19c8b8456040cd4998
mhadih <m.hadi.hojjat@gmail.com>
2019-02-04 13:04:52 +0330

    add Farsi

--- www/index.php
+++ www/index.php
@@ -29,7 +29,7 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-$p1 = GET['p'];
+$p1 = $_GET['p'];
 try {
     require_once('../includes/init.php');
     
