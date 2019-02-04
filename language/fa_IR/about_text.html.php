808ea5d38253b5a34baad051d4b1b6b99429fdf1
mhadih <m.hadi.hojjat@gmail.com>
2019-02-04 14:44:52 +0330

    add Farsi

--- language/fa_IR/about_text.html.php
+++ language/fa_IR/about_text.html.php
@@ -1,8 +1,7 @@
 <h1 class="text-right">Welcome to {cfg:site_name}</h1>
 
 <p>
-    {cfg:site_name} افزونه‌ایست برای FileSender (<a rel="nofollow" href="http://www.filesender.org/" target="_blank">www.filesender.org</a>),
-    که می توان به کمک آن، ملزومات جامعه تحصیل کرده را گسترش داد.
+    {cfg:site_name} افزونه است برای Filesender  کخ می توان با آن .
 </p>
 <p>
     Authentication of users is provided through <a rel="nofollow" href="http://simplesamlphp.org/" target="_blank">
