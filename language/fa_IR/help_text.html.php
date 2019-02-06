583c3ced8ddab9b9c0eabc60d5ea7dc0aa3019f2
mhadih <m.hadi.hojjat@gmail.com>
2019-02-06 09:00:12 +0330

    add Farsi

--- language/fa_IR/help_text.html.php
+++ language/fa_IR/help_text.html.php
@@ -34,11 +34,6 @@
 </p>
 
 <p>
-    As an authenticated user you should be able to upload files one or
-    more times and either have FileSender email the recipients after
-    your upload completes or provide you with a link to allow file
-    download. You should also be able to invite other researchers to
-    the system to upload one or more files as a guest.
 
     شما به عنوان کاربر احراز شده می‌توانید فایل‌ها را یک یا چند بار بارگذاری کنید و برای دانلود می‌توانید
     یک لینک برای گیرندگان آن فایل بفرستید. همچنین می‌توانید به سایز تحضیل کردگان دعوتنامه ارسال کنید.
@@ -46,11 +41,8 @@
 
 </p>
 
-<h3>Possible Download Size Limitations</h3>
 <h3>امکان دانلود با هر حجمی</h3>
 <p>
-    Any modern browser will download files of any size from the site.
-    Nothing special is required for downloads.
 
     تمام مرورگرهای جدید می‌توانند هر فایلی را با هر حجمی دانلود کنند
 </p>
@@ -58,14 +50,9 @@
 <h3>Pامکان بارکذاری با هر حجمی</h3>
 
 <p>
-    If your browser supports HTML5 then you should be able to upload
-    files of any size up to {size:cfg:max_transfer_size}. Current versions of Firefox and Chrome on
-    Windows, Mac OS X and Linux are known to have HTML5 support.
-
     اگر مرورگر شما از HTML5 پشتبانی می‌کند، شما می‌توانید هر فایلی را حداکثر به میزان {size:cfg:max_transfer_size} بارگذاری کنید.
 </p>
 
-<h3>Your browser's features</h3>
 <h3>ویژگی‌های مرورگر شما</h3>
 <ul class="fa-ul">
     <li data-feature="html5"><img src="images/html5_installed.png" alt="HTML5 upload enabled" /> شما می‌توانید هر فایلی‌ را با سایز ک.چکتر از {size:cfg:max_transfer_size} در هر انتقال بارگذاری کنید.</li>
@@ -74,49 +61,42 @@
 
 <h3>بارگذاری با هر اندازه‌ای با HTML5</h3>
 <ul class="fa-ul">
-    <li><i class="fa-li fa fa-caret-right"></i>You'll be able to use this method if the <img src="images/html5_installed.png" alt="HTML5 upload enabled" /> sign is displayed above</li>
     <li><i class="fa-li fa fa-caret-right"></i>شما می‌توانید از این امکان استفاده کنید اگر علامت <img src="images/html5_installed.png" alt="HTML5 upload enabled" /> نمایش داده شده باشد.</li>
-    <li><i class="fa-li fa fa-caret-right"></i>To enable this functionnality simply use an up to date browser that supports HTML5, the latest version of the "language of the web".</li>
     <li><i class="fa-li fa fa-caret-right"></i>برای استفاده از این امکان باید مرورگر شما بروز باشد و از HTML5 پشتیبانی کند.</li>
-    <li><i class="fa-li fa fa-caret-right"></i>Up to date versions of Firefox and Chrome on Windows, Mac OS X and Linux are known to work.</li>
     <li><i class="fa-li fa fa-caret-right"></i>مرورگر خود را بروز کنید</li>
     <li><i class="fa-li fa fa-caret-right"></i>
-        You can <strong>resume</strong> an interrupted or cancelled upload. To resume an upload, simply <strong>send the exact same files</strong> again !
-        Make sure the files have the <strong>same names and sizes</strong> as before.
-        When your upload starts, you should notice the progress bar jump to where the upload was halted, and continue from there.
-
 
         شما می‌توانید فایل‌هایی را که بارگذرای آنها به مشکل برخورده است را <strong>دوباره بارگذاری</strong> کنید. برای اینکار، آن‌ها را دوباره بارگذاری کنید
         برای ایکار مطمئن شوید که فایل‌ مورد نظر <strong>هم نام و هم اندازه</strong> فایل قبلی باشد.
+        هنگامی که بارگذاری دوباره شروع شد، باید میله‌پرشونده‌ به جایی بپرد که فبلا تا آنجا رفته بوده است.
     </li>
 </ul>
 
-<h3>Uploads up to {size:cfg:max_legacy_file_size} per file without HTML5</h3>
+<h3>بارگذاری حداکثر {size:cfg:max_legacy_file_size} بدون HTML5</h3>
 <ul class="fa-ul">
-    <li><i class="fa-li fa fa-caret-right"></i>FileSender will warn you should you try to upload a file that is too big for this method.</li>
-    <li><i class="fa-li fa fa-caret-right"></i>Resuming uploads is not supported with this method.</li>
+    <li><i class="fa-li fa fa-caret-right"></i>FileSender در چنین شرایطی به شما اخطار می‌دهد.</li>
+    <li><i class="fa-li fa fa-caret-right"></i>دوباره بارگذاری کردن در این روش امکان‌پذیر نیست.</li>
 </ul>
 
-<h3>Configured service constraints</h3>
+<h3>Cمشخصات سرویس</h3>
 <ul class="fa-ul">
-    <li><i class="fa-li fa fa-caret-right"></i><strong>Maximum number of recipients : </strong>{cfg:max_transfer_recipients} email addresses separated by a comma or semi-colon</li>
-    <li><i class="fa-li fa fa-caret-right"></i><strong>Maximum number of files per transfer : </strong>{cfg:max_transfer_files}</li>
-    <li><i class="fa-li fa fa-caret-right"></i><strong>Maximum size per transfer : </strong>{size:cfg:max_transfer_size}</li>
-    <li><i class="fa-li fa fa-caret-right"></i><strong>Maximum file size per file for non-HTML5 browsers : </strong>{size:cfg:max_legacy_file_size}</li>
-    <li><i class="fa-li fa fa-caret-right"></i><strong>Transfer expiry days : </strong>{cfg:default_transfer_days_valid} (max. {cfg:max_transfer_days_valid})</li>
-    <li><i class="fa-li fa fa-caret-right"></i><strong>Guest expiry days : </strong>{cfg:default_guest_days_valid} (max. {cfg:max_guest_days_valid})</li>
+    <li><i class="fa-li fa fa-caret-right"></i><strong>بیشینه تعداد گیرندگان: </strong>{cfg:max_transfer_recipients} ایمیل که با کاما و سمی‌کالن جدا می‌شوند. </li>
+    <li><i class="fa-li fa fa-caret-right"></i><strong>بیشینه تعداد فایل در هر انتقال:‌ </strong>{cfg:max_transfer_files}</li>
+    <li><i class="fa-li fa fa-caret-right"></i><strong>بیشنیه سایز در هر انتقال: </strong>{size:cfg:max_transfer_size}</li>
+    <li><i class="fa-li fa fa-caret-right"></i><strong>یبشینه سایز فایل در حالت بدون HTML5</strong>{size:cfg:max_legacy_file_size}</li>
+    <li><i class="fa-li fa fa-caret-right"></i><strong>تعداد روز انقضای انتقال: </strong>{cfg:default_transfer_days_valid} (نهایت: {cfg:max_transfer_days_valid})</li>
+    <li><i class="fa-li fa fa-caret-right"></i><strong>انقضای گواهی مهمان به روز:‌ </strong>{cfg:default_guest_days_valid} (نهایت: {cfg:max_guest_days_valid})</li>
 </ul>
 
 <h3>Technical details</h3>
 <ul class="fa-ul">
     <li><i class="fa-li fa fa-caret-right"></i>
-        <strong>{cfg:site_name}</strong> uses the <a href="http://www.filesender.org/" target="_blank">FileSender software</a>.
-        FileSender indicates whether or not the HTML5 upload method is supported for a particular browser.
-        This depends mainly on the availability of advanced browser functionality, in particular the HTML5 FileAPI.
-        Please use the <a href="http://caniuse.com/fileapi" target="_blank">"When can I use..."</a> website to monitor implementation progress of the HTML5 FileAPI for all major browsers.
-        In particular support for <a href="http://caniuse.com/filereader" target="_blank">FileReader API</a> and <a href="http://caniuse.com/bloburls" target="_blank">Blob URLs</a> needs to be light green (=supported) for a browser to support uploads larger than {size:cfg:max_legacy_file_size}.
-        Please note that although Opera 12 is listed to support the HTML5 FileAPI, it currently does not support all that is needed to support use of the HTML5 upload method in FileSender.
+        <strong>{cfg:site_name}</strong><a href="http://www.filesender.org/" target="_blank">FileSender software</a> استفاده می‌کند.
+        FileSender این مسئله را مرورگر شما بارگذاری با HTML5 را پشتیبانی می‌کند را خیر می‌گوید.
+        این قابیلیت از امکانات پیشترفته مروزگر‌ها استفاده می‌کند.
+        می‌توانید از صفحه‌ی <a href="http://caniuse.com/fileapi" target="_blank">"When can I use..."</a> برای دیدن تغییرات FileAPI در HTML5 استفاده کنید
+        برای اینکه ببینید چه ورژن‌هایی از مرورگرهااز این امکان پشتیبانی می‌کنند می توانید به <a href="http://caniuse.com/filereader" target="_blank">FileReader API</a> و <a href="http://caniuse.com/bloburls" target="_blank">Blob URLs</a> سر بزنید.
     </li>
 </ul>
 
-<p>For more information please visit <a href="http://www.filesender.org/" target="_blank">www.filesender.org</a></p>
+<p>اطلاعات بیشتر در: <a href="http://www.filesender.org/" target="_blank">www.filesender.org</a></p>
