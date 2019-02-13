73c2d80ea09a2793f8d662f397e5167ee7a5e58d
mhomidi <m.hadi.hojjat@gmail.com>
2019-02-13 14:32:09 +0330

    add Farsi

--- templates/logon_page.php
+++ templates/logon_page.php
@@ -6,6 +6,7 @@
         
         $embed = Config::get('auth_sp_embed');
         header('Location: '.AuthSP::logonURL(), true);
+        exit;
         if(!$embed) $embed = '<a id="btn_logon" href="'.AuthSP::logonURL().'">'.Lang::tr('logon').'</a>';
         
         echo $embed;
